affcc36ee3af720a7f2d4d2cfa2fc88e835f9a04
AntonLip <liplianin.anton@yandex.ru>
2020-05-02 12:07:57 +0300

    Start commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+ import React from 'react';
+ import './App.css';
+ import MyHeader from './MyHeader'
+ import Technologies from './Tecknologies'
+ const App = () => {
+   return (
+     <div>
+       <MyHeader />
+       <Technologies />
+       <div className="App">
+         <ul>
+           <li>
+             Hi
+               </li>
+         </ul>
+       </div>
+     </div>
+   );
+ }
+
+
+ export default App;
---
+++ src/MyHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const MyHeader = () =>
+{
+  return (
+      <div>
+        <a href="#">Home</a> -
+        <a href="#">Detail's</a> -
+        <a href="#">Exit</a> -
+      </div>
+  );
+}
+
+export default MyHeader;
