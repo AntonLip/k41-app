b208c8be012a487d691dee9cf8cd1faa1c100595
AntonLip <liplianin.anton@yandex.ru>
2020-07-15 18:49:38 +0300

    75

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,19 +3,42 @@ import React from 'react';
 import classes from './Login.module.css';
 
 
+const LoginFrorm = () => {
+    return (
+        <form>
+            <div className={classes.login}>
+                <input placeholder="Enter your email address." required />
+            </div>
+            <div className={classes.password}>
+                <input placeholder="Enter your password." required />
+            </div>
+            <div >
+                <input type={"checkbox"} placeholder="Enter your password." required />Remember me
+            </div>
+            <button className={classes.button}></button>
+        </form>
+    );
+}
+
+
+
+
 
 export class Login extends React.Component {
 
 
 
     componentDidMount() {
-      
+
     }
 
 
     render() {
         return (
             <div className={classes.okno}>
+                <LoginFrorm />
+            </div>
+           /* <div className={classes.okno}>
                 <div className={classes.aut} >
                     <div className={classes.text2}>Авторизация</div>
                     <div className={classes.login}>
@@ -25,13 +48,12 @@ export class Login extends React.Component {
                         <form action="Адрес сервера для отправки данных" method="post">
                             <input type="text" placeholder="Enter your password." required />
                         </form>
-                    </div>
+                    </div>      
                     <div >
-
                         <button className={classes.button}></button>
                     </div>
                 </div>
-            </div>);
+            </div>*/);
     }
 
 }
