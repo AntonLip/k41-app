4e2721ddd1aaa636191a82ba048729e34e5e2339
DmitriyKovchugo <dkovchugo@mail.ru>
2020-11-14 11:50:37 +0300

    lessons

--- src/Components/MyBody/Timetable/TimetableClass.jsx
+++ src/Components/MyBody/Timetable/TimetableClass.jsx
@@ -6,19 +6,18 @@ import { reduxForm, Field } from 'redux-form';
 
 
 export class Timetable extends React.Component {
- 
 
     componentDidMount() {
         this.props.getUser();
         var day = new Date().getDay();
         var month = new Date().getMonth();
         var year = new Date().getFullYear();
-        var fullDate = year + "-" + month + "-" + day + " 00:00:00.0000000"        
+        var fullDate = year + "-" + month + "-" + day + " 00:00:00.0000000"
         this.props.getTimetable(this.props.currGroup, fullDate);
     }
 
     getTT = (group, fullDate) => {
-       
+
         this.props.getTimetable(group, fullDate);
     }
 
@@ -26,15 +25,23 @@ export class Timetable extends React.Component {
         // print the form values to the console
         var fullDate = values.date + " 00:00:00.0000000";
         var group = values.group;
-       
+
         console.log(values)
         this.getTT(group, fullDate)
     }
-    render() {        
+    render() {
         return (
-            <div>
-                <TimetableReduxForm onSubmit={this.submit} role={this.props.user.profile}/>
-                <TT timetable={this.props.timetable} />
+        <div>
+            
+            <div className={classes.gridMain}>
+                <div className={classes.gridLeftSide}>
+                    <TimetableReduxForm onSubmit={this.submit} role={this.props.user.profile} />
+                </div>
+
+                <div className={classes.container}>
+                    <TT timetable={this.props.timetable} />
+                </div>
+            </div>
             </div>
         );
     }
@@ -47,13 +54,17 @@ const TT = (props) => {
             {
                 props.timetable.map(u => {
                     return (
-
+<div className={classes.AllLessons}>
                         <div className={classes.card__lessons} key={u.id}>
                             <div className={classes.card__lesson_item}>
                                 <div className={classes.card__lesson_number}>{u.numberOfLessonInDay}</div>
                                 <div className={classes.titles}>
                                     <div className={classes.card__lesson_title}>{u.nameOfDiscipline}</div>
+                                    
+                        <div className={classes.card__lesson_item2}>
                                     <div className={classes.card__lesson_title}>{u.typeOfLesson}</div>
+                                    <div className={classes.card__lesson_title2}>{u.numberOfLesson}</div>
+                        </div>
                                     <div className={classes.card__lesson_title}>{u.auditore}</div>
                                     <div className={classes.card__lesson_title}>{u.lectural}</div>
                                 </div>
@@ -62,6 +73,13 @@ const TT = (props) => {
                                     <input className={classes.card__lesson_more} type="text" required placeholder="просьба преподавателю"></input>
                                 </div>
                             </div>
+                        </div>
+                        <div>
+                            <button className={classes.send} type="submit">ввод</button>
+                        </div>
+       <div className={classes.enter}>
+<a href="#">Подробнее</a>
+</div>
                         </div>
                     );
                 }
@@ -74,7 +92,7 @@ const TT = (props) => {
 
 
 const TimetableFormRedux = (props) => {
-   
+
     return (
         <form onSubmit={props.handleSubmit}>
             <div >
