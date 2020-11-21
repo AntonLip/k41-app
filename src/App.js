4452bd16d6f0841b865e99b0956c67819a4bc505
AntonLip <liplianin.anton@yandex.ru>
2020-11-21 11:55:00 +0300

    Delete eduPart

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { MyNewsContainer } from './Components/MyBody/MyNews/MyNewsComponenta';
 import UsersContainer from './Components/People/Lectural/UsersContainer';
 import ProfileComponenta from './Components/People/Lectural/ProfileLectural/ProfileComponenta';
 import LoginSuccessContainer from './Components/Login/LoginContainer';
-import { EducationPart } from './Components/MyBody/EducationalPart/EducationalPart';
 import NewUserContainer from './Components/People/Lectural/newLectural/newLecturalContainer'
 import { HistoryOfChief } from './Components/MyBody/Chief/History/history';
 import  ChiefComponent  from './Components/MyBody/Chief/chiefComponent';
@@ -23,19 +22,26 @@ const App = (props) => {
 
       <div className='app_wrapper_content'>
         <Route exact path='/'  render={() => <Redirect to={"/MyNews"}/>}/>
-        <Route path="/EducationPage" render={() => { return <EducationPart/> }} />
+        <Route path="/MyNews" render={() => { return <MyNewsContainer /> }} />
+
+        <Route path="/Timetable"  render={()=>{return <TimetibleContainer />} } />
+
         <Route path="/Chief" render={() => { return <ChiefComponent/> }} />
 
-        <Route path="/MyNews" render={() => { return <MyNewsContainer /> }} />
         <Route path="/Users" render={()=>{return <UsersContainer/>}}/>
-        <Route path="/Profile/:id"  render={()=>{return <ProfileComponenta />} } />
-        <Route path="/signin-oidc"  render={()=>{return <LoginSuccessContainer />} } />
-        <Route path="/Timetable"  render={()=>{return <TimetibleContainer />} } />
-        <Route path="/Cadets" render={()=>{return <CadetsComponent />} }/>
-        <Route path="/History" render={()=>{return <HistoryOfChief />} }/>
         <Route path="/newUser" render={()=>{return <NewUserContainer />} }/>
         <Route path="/updateUser/:id" render={()=>{return <UpdateUserContainer />} }/>
+        <Route path="/Profile/:id"  render={()=>{return <ProfileComponenta />} } />
+
+        <Route path="/Cadets" render={()=>{return <CadetsComponent />} }/>
+
+        <Route path="/History" render={()=>{return <HistoryOfChief />} }/>
+
         <Route path="/Сlassrooms" render={()=>{return <СlassroomsContainer/>} }/>
+
+
+        <Route path="/signin-oidc"  render={()=>{return <LoginSuccessContainer />} } />
+
       </div>
       <Footer />
     </div>
