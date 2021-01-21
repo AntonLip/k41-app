f49e7b3afc78ef988e27dc771535b6e64b058f53
Warhunt <pochtarossii123456789@gmail.com>
2021-01-21 09:39:18 +0300

    after merge 21.01.2021 9.39

--- src/Components/Body/Who/WhoEdit/WhoEdit.jsx
+++ src/Components/Body/Who/WhoEdit/WhoEdit.jsx
@@ -3,20 +3,7 @@ import { GrLinkPrevious } from 'react-icons/gr'
 import { Link, withRouter } from 'react-router-dom'
 import { compose } from 'redux'
 import MainContentWrapper from '../../MainContentWrapper/MainContentWrapper'
-<<<<<<< HEAD
-import { connect } from 'react-redux'
-import { getFilteredofficersThunkCreator, getofficersThunkCreator, getCadetsThunkCreator, getCadetsByIdThunkCreator } from '../../../../Redux/whos-reduser'
-import InputDropDown from '../../Sort/Items/Input/items/InputDropDown/InputDropDown'
-import TextArea from '../../Sort/Items/Input/items/TextArea/TextArea'
-import { InputDate } from '../../Sort/Items/Date/InputDate'
-import InputFile from '../../Sort/Items/Input/items/File/InputFile'
-import { CheckboxArea } from '../../Sort/Items/Input/items/Checkdox/InputCheckbox'
-import { initialize, reduxForm } from 'redux-form'
-import { setAcademicDegreeThunkCreator, setAcademicTitleThunkCreator, setGroupsThunkCreator, setMilitaryRankThunkCreator, setUnitThunkCreator } from '../../../../Redux/generalInfo-reduser'
-import TextInput from '../../Sort/Items/Input/items/TextInput/TextInput'
-=======
 import WhoEditForm from './WhoEditForm'
->>>>>>> 04328c62a94334238daeaaae65bd81ec4b4e88ca
 
 class WhoEdit extends React.Component {
 
